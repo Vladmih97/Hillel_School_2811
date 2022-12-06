d73e1848071af9ba3bcd2087323bd5d00d90cc57
VladMih <vladmih97@gmail.com>
2022-12-06 19:56:07 +0200

    3 strings printed

--- Homework#1.py
+++ Homework#1.py
@@ -1,8 +1,17 @@
+antigravity = "\t\timport antigravity"
+helloString = "\n\t\timport__hello__"
+futureString = "\n\t\tfrom __future__ import braces"
+print(antigravity + helloString + futureString)
+
 from PIL import Image, ImageDraw
-im=Image.new('1',(80, 24), color=1)
-draw=ImageDraw.Draw(im)
-draw.text((5,5), 'V L A D')
+
+im = Image.new('1', (80, 24), color=1)
+draw = ImageDraw.Draw(im)
+draw.text((5, 5), 'V L A D')
 for i in range(im.height):
-     for j in range(im.width):
-         print ('* '[im.getpixel((j,i))], end='')
-     print()
+    for j in range(im.width):
+        print('* '[im.getpixel((j, i))], end='')
+    print()
+
+
+
