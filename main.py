b4920ea29c31e1f36c2c8dcbeb01ad3fb77f2bcd
VladMih <vladmih97@gmail.com>
2022-12-06 18:51:30 +0200

    Типы данных

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ print(variable)
 variable = 'Vlad'
 print(variable)
 
-apples_in_box = 99 #snake_case
-applesInBox = 88 #camelCase
+apples_in_box = 99  # snake_case
+applesInBox = 88  # camelCase
 # Ctrl+Alt+L <- автоматическое форматирование кода по pep8
+
+i = int() # целые числа
+f = float() # дробные числа (разделяются точкой)
+s = str() # строки
+b = bool # логические данные (истина/ложь(True/False))
