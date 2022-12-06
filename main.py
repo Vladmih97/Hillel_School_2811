310a4afac74ccc7ba8f0ffe305053fd85e367a8a
VladMih <vladmih97@gmail.com>
2022-12-06 18:41:28 +0200

    Пременные, именование

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("hello")
+variable = 87
+print(variable)
+variable = 'Vlad'
+print(variable)
+
+apples_in_box = 99 #snake_case
+applesInBox = 88 #camelCase
+# Ctrl+Alt+L <- автоматическое форматирование кода по pep8
